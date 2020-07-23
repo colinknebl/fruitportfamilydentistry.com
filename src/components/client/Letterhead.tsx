20780199fc8e60763ca13d33b3b9eb08805ead12
Colin Knebl <colinknebl@outlook.com>
2020-07-22 20:19:58 -0400

    add client letterhead

--- src/components/client/Letterhead.tsx
+++ src/components/client/Letterhead.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
+import { FluidObject } from 'gatsby-image';
 
 import { getDoctors } from '../../gql/queries/doctors';
+import { Image } from '../image';
 
 interface ILetterheadProps {
     date?: string;
 }
 
+interface IImageQueryResults {
+    placeholderImage: {
+        childImageSharp: {
+            fluid: FluidObject;
+        };
+    };
+}
+
 export function Letterhead({ date }: ILetterheadProps) {
     const { doctors } = getDoctors();
+    const image = useStaticQuery<IImageQueryResults>(graphql`
+        query {
+            placeholderImage: file(relativePath: { eq: "letterhead.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 60) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `);
+
     return (
         <>
             <StyledHeader>
                 <div>
+                    <Image
+                        style={{
+                            width: '60px',
+                            height: '51px',
+                        }}
+                        alt="logo"
+                        fluid={image.placeholderImage.childImageSharp.fluid}
+                    />
                     <span className="practice-name">
                         Grand Haven Family Dentistry
                     </span>
@@ -24,9 +55,14 @@ export function Letterhead({ date }: ILetterheadProps) {
                 </div>
                 <div>
                     <address>
-                        <span>919 S. Beechtree, Ste. #7</span>
-                        <span>Grand Haven, MI 49417</span>
-                        <span>(616) 844-4400</span>
+                        <span>40 E. Beech St.</span>
+                        <span>Fruitport, MI 49415</span>
+                        <span>(231) 865-6141</span>
+                        <span>
+                            <a href="https://fruitportfamilydentistry.com">
+                                FruitportFamilyDentistry.com
+                            </a>
+                        </span>
                     </address>
                 </div>
             </StyledHeader>
@@ -39,6 +75,7 @@ export function Letterhead({ date }: ILetterheadProps) {
 const StyledHeader = styled.header`
     display: grid;
     grid-template-columns: 1fr 1fr;
+    align-items: end;
 
     & > div:nth-child(2) {
         text-align: right;
